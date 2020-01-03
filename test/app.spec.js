f013e35d3c08a114ce2802a004d1970a291cde3d
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-03 14:19:22 -0500

    added post test to db. working

--- test/app.spec.js
+++ test/app.spec.js
@@ -4,7 +4,7 @@ const supertest = require('supertest');
 const app = require('../src/app')
 
 describe('APP', () => {
-    let db
+    let workoutsCopy, db;
     before('make knex instance', () => {
         db = knex({
           client: 'pg',
@@ -13,12 +13,32 @@ describe('APP', () => {
         app.set('db', db);
       });
 
+    after('disconnect from db', () => db.destroy());
+
+    before('cleanup', () => db('workouts').truncate());
+
+    afterEach('cleanup', () => db('workouts').truncate());
+
 
     it('GET / responds with 200', () => {
         return supertest(app)
         .get('/workouts')
         .expect('Content-type', /json/)
         .expect(200)
-
     })
+
+    it('creates a workout, responding with 201 and the new workout', () => {
+        return supertest(app)
+          .post('/workouts')
+          .send({
+              title: 'test new workout',
+              workout1: 'name of workout',
+              lbs: 50,
+              set1: 10,
+              set2: 10,
+              set3: 10
+          })
+          .expect(201)
+    })
+
 })

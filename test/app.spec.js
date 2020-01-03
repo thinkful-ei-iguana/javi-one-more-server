41effde702c434b0d758e35f0d3b9c83f31b6c89
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-03 13:53:27 -0500

    added test table

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,7 +8,7 @@ describe('APP', () => {
     before('make knex instance', () => {
         db = knex({
           client: 'pg',
-          connection: process.env.DB_URL,
+          connection: process.env.TEST_DB_URL,
         });
         app.set('db', db);
       });

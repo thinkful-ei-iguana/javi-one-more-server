3d3b7a5cb295747fff7dfb13e3fcff5f403090de
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-14 15:33:14 -0500

    got rid of previous change

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -6,7 +6,7 @@ const authRouter = express.Router()
 const jsonBodyParser = express.json()
 
 authRouter
-  .post('workouts/login', jsonBodyParser, (req,res,next) => {
+  .post('/login', jsonBodyParser, (req,res,next) => {
       const { user_name, password } = req.body
       const loginUser = { user_name,password }
 
